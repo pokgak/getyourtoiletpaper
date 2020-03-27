c6f7b198a2e9acdfb33fbc98cc7a96b1e9fce327
pokgak <m.aimanismail@gmail.com>
2020-03-27 23:38:04 +0100

    backend: seed items in db

---
+++ backend/database/seeds/ItemsTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Item::class, 10)->create();
+    }
+}
