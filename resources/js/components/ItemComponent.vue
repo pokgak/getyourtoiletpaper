275b28d97da364596fa52fe92daa659dfc22ec3c
pokgak <m.aimanismail@gmail.com>
2020-03-28 21:28:49 +0100

    frontend: connect to backend

--- resources/js/components/ItemComponent.vue
+++ resources/js/components/ItemComponent.vue
@@ -1,23 +1,80 @@
 <template>
-    <div class="card mb-4">
+    <div class="card mb-4" v-show="!this.isDeleted">
         <img src="http://www.staples-3p.com/s7/is/image/Staples/m005185536_sc7?$std$" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">{{ item.name }}</h5>
-            <p class="card-text">{{ item.description }}</p>
 
+        <!-- Display mode -->
+        <div v-show="!editMode">
+            <div class="card-body">
+                <h5 class="card-title">{{ item.name }}</h5>
+                <p class="card-text">{{ item.description }}</p>
+
+            </div>
+            <div class="card-footer text-right">
+                <button type="button" class="btn btn-primary" v-on:click='editItem'><i class="fa fa-pencil-square-o fa-lg" title="Edit Item"></i></button>
+                <button type="button" class="btn btn-danger" v-on:click='deleteItem'><i class="fa fa-trash-o fa-lg" title="Delete Item"></i></button>
+            </div>
         </div>
-        <div class="card-footer text-right">
-            <a href="#" class="btn btn-primary"><i class="fa fa-pencil-square-o fa-lg" title="Edit Item"></i></a>
-            <a href="#" class="btn btn-danger"><i class="fa fa-trash-o fa-lg" title="Delete Item"></i></a>
+
+        <!-- Edit mode only -->
+        <div v-show="editMode">
+            <div class="card-body">
+                <form>
+                    <input type="text" id="name" class="form-control card-title" v-model="item.name">
+                    <textarea id="description" class="form-control card-text" v-model="item.description" rows="7"></textarea>
+                </form>
+            </div>
+            <div class="card-footer text-right">
+                <button type="submit" class="btn btn-success" v-on:click='submit'><i class="fa fa-check fa-lg" title="Submit"></i></button>
+                <button type="reset" class="btn btn-danger" v-on:click='reset'><i class="fa fa-times-circle fa-lg" title="Reset"></i></button>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    var baseApiUrl = "http://homestead.test/api"
+
     export default {
         props: ['item'],
+        data: function () {
+            return {
+                isDeleted: false,
+                editMode: false,
+                oldItem: {
+                    'name': this.item.name,
+                    'description': this.item.description
+                }
+            }
+        },
         mounted() {
             console.log('Component mounted.')
+        },
+        methods: {
+            print: function () {
+                console.log(this.item.id)
+            },
+            editItem: function () {
+                console.log("EDIT!" + this.item.id)
+                this.oldItem = this.item
+                this.editMode = true
+            },
+            deleteItem: function () {
+                console.log("DELETE!" + this.item.id)
+                // TODO: confirm popup
+                axios.delete(baseApiUrl + "/item/" + this.item.id)
+                this.isDeleted = true
+            },
+            submit: function () {
+                console.log("SUBMIT!" + this.item.id)
+                axios.put(baseApiUrl + "/item", this.item)
+
+                this.editMode = false
+            },
+            reset: function () {
+                console.log("RESET!" + this.item.id)
+                this.item = this.oldItem
+                this.editMode = false
+            }
         }
     }
 </script>
