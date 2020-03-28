118fca7a2dfda5d81c197dd1d08e1270370e795b
pokgak <m.aimanismail@gmail.com>
2020-03-28 02:43:42 +0100

    db: seed users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\User::class)->create();
+
         $this->call(ItemsTableSeeder::class);
     }
 }
