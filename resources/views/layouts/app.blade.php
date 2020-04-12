c329f282dd7342a0e66b652aebc6056d0a45d287
pokgak <m.aimanismail@gmail.com>
2020-04-12 13:34:35 +0200

    add cart icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,6 +52,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" role="button" title="View cart"><i class="fa fa-lg fa-shopping-cart"></i></a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
